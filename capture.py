c8bf63c1a193ae3d83597504b070ecf46f003b8e
isaacshannon <isaac.shannon@gmail.com>
2019-12-10 19:45:23 -0500

    save

--- capture.py
+++ capture.py
@@ -1,6 +1,11 @@
 from subprocess import call
 from time import sleep
 import os
+import random
+from datetime import datetime
+from shutil import copyfile
+
+random.seed(datetime.now())
 
 def make_dirs(directory):
     try:
@@ -19,16 +24,26 @@ def make_dirs(directory):
         pass
 
 def capture():
-    teams = "dfm_spy_worlds_2019"
+    teams = "spy_uol_6_2019"
     directory = "games/" + teams
     make_dirs(directory)
+    random.seed()
 
-    sleep(5)
+    # sleep(1)
 
-    for i in range(1200):
+    for i in range(1600):
+        j0 = str(random.randrange(0, 6))
+        j1 = str(random.randrange(2, 8))
+        j2 = str(random.randrange(46, 54))
+        j3 = str(random.randrange(76, 84))
         seq = format(i, '010d')
-        call(["screencapture",  "-R815,665,150,150", "-x", f"{directory}/full/{teams}_{seq}.png"])
-        call(["screencapture", "-R1770,675,150,150", "-x", f"{directory}/team/{teams}_{seq}.png"])
+        call(["screencapture",  f"-R80{j0},57{j1},150,150", "-x", f"{directory}/full/{teams}_{seq}.png"])
+        call(["screencapture", f"-R17{j2},5{j3},150,150", "-x", f"{directory}/team/{teams}_{seq}.png"])
         sleep(1)
+
+    files = os.listdir(directory+"/full")
+    fileIndexes = random.sample(range(0, len(files)), 10)
+    for i in fileIndexes:
+        copyfile(directory+"/full/"+files[i], "coordinates/unlabeled/"+files[i])
 
 capture()
--- coordinates/splitter.py
+++ coordinates/splitter.py
@@ -1,13 +1,13 @@
 from PIL import Image
 import os
 import csv
+import os, shutil
+import random
 
-
-
+# The splitter separates the images which have player coordinates into 10x10px images for training.
 def split(filename, player_grids, k):
     s = 0
     grids = [int(i) for i in player_grids.split(" ")]
-    print(grids)
 
     height = 10
     width = 10
@@ -19,17 +19,69 @@ def split(filename, player_grids, k):
             a = im.crop(box)
             try:
                 type = "terrain"
+                set = "train"
+                if random.randrange(0, 5) < 1:
+                    set = "valid"
                 if (j*1000/width + i//height) in grids:
                     type = "player"
-                a.save(f"./split/{type}/sp-{k}{s}.png")
+                a.save(f"./split/{set}/{type}/sp-{k}{s}.png")
             except:
                 pass
             s += 1
+
+
+# Clear the existing files
+folder = './split'
+for filename in os.listdir(folder):
+    file_path = os.path.join(folder, filename)
+    try:
+        if os.path.isfile(file_path) or os.path.islink(file_path):
+            os.unlink(file_path)
+        elif os.path.isdir(file_path):
+            shutil.rmtree(file_path)
+    except Exception as e:
+        print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+print("starting splitter...")
+
+try:
+    os.mkdir("./split/train")
+except:
+    pass
+try:
+    os.mkdir("./split/train/terrain")
+except:
+    pass
+try:
+    os.mkdir("./split/train/player")
+except:
+    pass
+
+try:
+    os.mkdir("./split/valid")
+except:
+    pass
+try:
+    os.mkdir("./split/valid/player")
+except:
+    pass
+try:
+    os.mkdir("./split/valid/terrain")
+except:
+    pass
 
 k = 0
 with open('./coordinates.csv', 'r') as csvfile:
     reader = csv.reader(csvfile, delimiter=',', quotechar='|')
     next(reader)
     for r in reader:
-        split(r[0], r[1], k)
+        try:
+            split(r[0], r[1], k)
+        except:
+            print(f"invalid row: {r}")
         k+=1
+
+        if k%50 == 0:
+            print(k)
+
+print("done splitting!")
