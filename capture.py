55f18cd3186203e42f6b20cdfab082b14a345214
isaacshannon <isaac.shannon@gmail.com>
2020-05-03 15:05:22 -0400

    a bit of cleanup

--- capture.py
+++ capture.py
@@ -1,4 +1,3 @@
-import time
 from time import sleep
 import os
 import pyscreenshot
@@ -27,8 +26,8 @@ def make_dirs(directory):
 
 
 def capture():
-    teams = "lec_2020_mad_xl"
-    num_shots = 1400
+    teams = "lcs_2020_jan26_dig_eg"
+    num_shots = 1200
     directory = "games/" + teams
     make_dirs(directory)
     if len(os.listdir(directory)) > 100:
@@ -39,16 +38,16 @@ def capture():
     for i in range(num_shots):
         seq = format(i, '010d')
 
-        x = 1440
+        x = 1471
         min_x = random.randrange(x, x+6)
-        y = 765
+        y = 763
         min_y = random.randrange(y, y+6)
         im_full = pyscreenshot.grab(bbox=(min_x, min_y, min_x + 150, min_y + 150))
         im_full.save(f"{directory}/full/{teams}_{seq}.png")
 
-        x = 1263
+        x = 1280
         min_x = random.randrange(x, x + 6)
-        y = 720
+        y = 770
         min_y = random.randrange(y, y + 6)
         im_team = pyscreenshot.grab(bbox=(min_x, min_y, min_x + 150, min_y + 150))
         im_team.save(f"{directory}/team/{teams}_{seq}.png")
